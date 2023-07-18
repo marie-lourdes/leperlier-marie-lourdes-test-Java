43241fbdd476c810187a3bb999a164b9b9f39dd3
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-07-19 00:16:22 +0200

    DataBaseTestConfig: add try catch to test th problem of connection

--- src/test/java/com/parkit/parkingsystem/integration/config/DataBaseTestConfig.java
+++ src/test/java/com/parkit/parkingsystem/integration/config/DataBaseTestConfig.java
@@ -1,18 +1,30 @@
 package com.parkit.parkingsystem.integration.config;
 
-import com.parkit.parkingsystem.config.DataBaseConfig;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.sql.*;
+import com.parkit.parkingsystem.config.DataBaseConfig;
 
 public class DataBaseTestConfig extends DataBaseConfig {
 
     private static final Logger logger = LogManager.getLogger("DataBaseTestConfig");
 
     public Connection getConnection() throws ClassNotFoundException, SQLException {
-        logger.info("Create DB connection");
-        Class.forName("com.mysql.cj.jdbc.Driver");
+    	try {
+    		 logger.info("Create DB connection");
+    	        Class.forName("com.mysql.cj.jdbc.Driver");
+    	
+    	}catch(Exception e) {
+    		System.err.println("erreur connexion");
+    		//throw new Exception("erreur connexion");
+    		e.printStackTrace();
+    	}
         return DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/test","root","rootroot");
     }
