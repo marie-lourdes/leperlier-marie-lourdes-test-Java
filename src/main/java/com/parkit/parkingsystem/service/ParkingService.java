969804a3bf22ac1a0fbe1103d0c7c9630e72263a
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-07-19 09:28:01 +0200

    ParkingService : delete comment arount ticket.setId() add id 0

--- src/main/java/com/parkit/parkingsystem/service/ParkingService.java
+++ src/main/java/com/parkit/parkingsystem/service/ParkingService.java
@@ -40,7 +40,8 @@ public class ParkingService {
 				Date inTime = new Date();
 				Ticket ticket = new Ticket();
 				// ID, PARKING_NUMBER, VEHICLE_REG_NUMBER, PRICE, IN_TIME, OUT_TIME)
-				ticket.setId(parkingSpot.getId());
+				int ticketID = 0;
+				ticket.setId(ticketID);
 				ticket.setParkingSpot(parkingSpot);
 				ticket.setVehicleRegNumber(vehicleRegNumber);
 				ticket.setPrice(0);
