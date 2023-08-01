bcd3d5a74f9ec75b8d809af4e86ded32996dbe78
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-08-01 20:23:38 +0200

    ParkingSpotDAO: delete throws exception with method  getNextAvailableSlot and updateParking

--- src/main/java/com/parkit/parkingsystem/dao/ParkingSpotDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/ParkingSpotDAO.java
@@ -17,7 +17,7 @@ public class ParkingSpotDAO {
 
     public DataBaseConfig dataBaseConfig = new DataBaseConfig();
 
-    public int getNextAvailableSlot(ParkingType parkingType) throws SQLException{
+    public int getNextAvailableSlot(ParkingType parkingType){
         Connection con = null;
         int result=-1;
         try {
@@ -38,7 +38,7 @@ public class ParkingSpotDAO {
         return result;
     }
 
-    public boolean updateParking(ParkingSpot parkingSpot) throws SQLException{
+    public boolean updateParking(ParkingSpot parkingSpot){
         //update the availability fo that parking slot
         Connection con = null;
         try {
