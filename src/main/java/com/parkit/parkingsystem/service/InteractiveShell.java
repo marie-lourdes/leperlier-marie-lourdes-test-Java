82d799cb1857ed4c8b753ff9bc9cf98b49a950c6
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-08-03 08:04:23 +0200

    InteractiveShell: modify switch with new swith java8 with expression lambda and modifu pom.xml with java 17

--- src/main/java/com/parkit/parkingsystem/service/InteractiveShell.java
+++ src/main/java/com/parkit/parkingsystem/service/InteractiveShell.java
@@ -23,21 +23,14 @@ public class InteractiveShell {
         while(continueApp){
             loadMenu();
             int option = inputReaderUtil.readSelection();
-            switch(option){
-            case 1: {
-                parkingService.processIncomingVehicle();
-                break;
-            }
-            case 2: {
-                parkingService.processExitingVehicle();
-                break;
-            }
-            case 3: {
-                System.out.println("Exiting from the system!");
-                continueApp = false;
-                break;
-            }
-            default: System.out.println("Unsupported option. Please enter a number corresponding to the provided menu");
+            switch(option) {
+	            case 1 -> parkingService.processIncomingVehicle();    
+	            case 2 -> parkingService.processExitingVehicle();
+	            case 3 -> {
+	                System.out.println("Exiting from the system!");
+	                continueApp = false;    
+	            }
+	            default-> System.out.println("Unsupported option. Please enter a number corresponding to the provided menu");       
             }
         }
     }
