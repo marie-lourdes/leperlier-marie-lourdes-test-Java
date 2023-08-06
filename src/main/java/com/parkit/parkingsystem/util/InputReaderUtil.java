689a70c48e3b99fa0f03161baee2246b47809eab
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-08-06 14:00:31 +0200

    InputReaderUtil.java: replace println by  logger.error in methods in bloc catch

--- src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
+++ src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
@@ -14,7 +14,7 @@ public class InputReaderUtil {
             return Integer.parseInt(scan.nextLine());
         }catch(Exception e){
             logger.error("Error while reading user input from Shell", e);
-            System.out.println("Error reading input. Please enter valid number for proceeding further");
+            logger.error("Error reading input. Please enter valid number for proceeding further");
             return -1;
         }
     }
@@ -28,7 +28,7 @@ public class InputReaderUtil {
             return vehicleRegNumber;
         }catch(Exception e){
             logger.error("Error while reading user input from Shell", e);
-            System.out.println("Error reading input. Please enter a valid string for vehicle registration number");
+            logger.error("Error reading input. Please enter a valid string for vehicle registration number");
             throw e;
         }
     }
